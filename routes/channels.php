c28dac29e55fc47074a4fed2ca37d3ee1d9f7958
Georgiy Olar <avtovogol@ya.ru>
2022-05-04 20:21:05 +0300

    SM-1 basic implementation

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    //Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    //    return (int) $user->id === (int) $id;
+    //});
+
+Broadcast::channel('room.{room_id}', function ($user, $room_id) {
+    if($user->rooms->contains($room_id)){
+        return $user->name;
+    }
 });
