a17df1830d920443e15a1c97186cfdc70207f75f
Georgiy Olar <avtovogol@ya.ru>
2022-05-06 05:57:06 +0300

    SM-3 app name change

--- public/js/app.js
+++ public/js/app.js
@@ -5567,7 +5567,7 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "local",
   forceTLS: false,
-  wsHost: '127.0.0.1',
+  wsHost: "127.0.0.1",
   wsPort: 6001,
   encrypted: false
 });
